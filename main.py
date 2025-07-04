77675f2c38345f7c51e72479e91d9f7ac79ca0f5
ahmadelsayedallam <121812595+ahmadelsayedallam@users.noreply.github.com>
2025-07-04 20:02:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,4 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 # تشغيل التطبيق
 if __name__ == "__main__":
     init_db()
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
     app.run_polling()
